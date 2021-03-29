343258deed3b09f9c6d2ed40fcf251a0aa3299fb
Ivaylo Petrov <ivaylo020@gmail.com>
2021-03-29 19:00:33 +0300

    create Login page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import pages from './pages';
 import './App.scss';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={pages.Home} />
+        <Route path="/login" component={pages.Login} />
       </Switch>
     </Router>
   );
