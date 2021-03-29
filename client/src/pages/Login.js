cee0e45b79875fb435915b2b48589f6f1e481325
Ivaylo Petrov <ivaylo020@gmail.com>
2021-03-29 19:33:07 +0300

    add header to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,10 @@
+import Header from '../components/Header';
 import LoginForm from '../components/LoginForm';
 
 const Login = () => {
     return (
         <div className="login-page">
+            <Header />
             <LoginForm />
         </div>
     );
