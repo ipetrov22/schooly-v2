117dd519c2a703f16f5b489b78eade0c756637a2
Ivaylo Petrov <ivaylo020@gmail.com>
2021-03-29 23:26:48 +0300

    style login form

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { MdLock } from 'react-icons/md';
 import './LoginForm.scss';
 
@@ -7,13 +8,38 @@ const LoginForm = () => {
             <form className="login-form">
                 <article className="icon-container">
                     <MdLock size={'35px'}
-                        style={{ background: '#3f51b5', fill: '#fff', borderRadius: '50%', padding: '5px' }} />
+                        style={{ background: '#3f51b5', fill: '#fff', borderRadius: '50%', padding: '7px' }} />
                 </article>
 
                 <h2 className="title">Login</h2>
 
-                <input className="input-field" autoComplete="email" type="email" htmlFor="email" name="email" required />
+                <input className="input-field"
+                    autoComplete="email"
+                    type="email"
+                    htmlFor="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                />
+
+                <input className="input-field"
+                    type="password"
+                    htmlFor="password"
+                    наме="password"
+                    placeholder="Password"
+                    required
+                />
+
+                <button className="submit-btn" type="submit">
+                    Login
+                </button>
+
+
+                <Link to="/register">
+                    <p className="link">Don't have an account? Register here.</p>
+                </Link>
             </form>
+
         </div>
     );
 };
