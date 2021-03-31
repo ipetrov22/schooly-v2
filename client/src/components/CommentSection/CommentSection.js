5e4dbac59994119b19f5ce488f6209a34488759a
Ivaylo Petrov <ivaylo020@gmail.com>
2021-03-31 22:45:29 +0300

    add comment field

--- client/src/components/CommentSection/CommentSection.js
+++ client/src/components/CommentSection/CommentSection.js
@@ -1,9 +1,15 @@
 import './CommentSection.scss';
 
 const CommentSection = () => {
+
     return (
         <div className="comment-section-wrapper">
             <h2 className="title">Comments</h2>
+
+            <textarea
+                className="comment-input"
+                placeholder="Comment..."
+            />
         </div>
     );
 };
