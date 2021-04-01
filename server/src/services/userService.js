dfd28fec29c64b8885fbc36d363a49cbac329b27
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-01 16:22:31 +0300

    create createUser service

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -1,7 +1,25 @@
 const admin = require('../config/firebase');
+const UserModel = require('../models/UserModel');
 
-const createUser = ({ username, email, password }) => {
+const createUser = async ({ username, email, password }) => {
+    const user = await UserModel.findOne({ username });
 
+    if (user) {
+        throw 'Username is taken!';
+    }
+
+    return admin.auth().createUser({ email, password })
+        .then(async (userRecord) => {
+            const user = new UserModel({ username, email });
+            const _id = user._id;
+            await user.save();
+
+            return admin.auth().setCustomUserClaims(userRecord.uid, { _id, username });
+        })
+        .then(() => 'User created.')
+        .catch((err) => {
+            throw err;
+        });
 };
 
 module.exports = {
