30f9c79e71e11007641626d6e24f54741008f02e
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-01 22:04:41 +0300

    create email validator helper

---
+++ client/src/helpers/validators.js
@@ -0,0 +1,7 @@
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const emailValidator = (email) => {
+    if (!email.match(emailRegex)) {
+        return 'Invalid email';
+    }
+};
