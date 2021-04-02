e0ce1120d3a64e751c57e38fb6d12209235ce9fd
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-02 16:28:05 +0300

    add menu button in header for auth users

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
+import { MdMenu } from 'react-icons/md';
+import { connect } from 'react-redux';
 import './Header.scss';
 
-const Header = () => {
+const Header = ({ firebaseUser }) => {
     return (
         <header className="app-header-wrapper">
             <div className="header-container">
@@ -9,22 +11,32 @@ const Header = () => {
                     <button className="logo-title">Schooly</button>
                 </Link>
 
-                <section className="auth-buttons">
-                    <Link to="/login">
-                        <button className="auth-btn">
-                            Login
-                        </button>
-                    </Link>
+                {firebaseUser ?
+                    <button className="menu-btn">
+                        <MdMenu size={30} fill={'#fff'}/>
+                    </button>
 
-                    <Link to="/register">
-                        <button className="auth-btn">
-                            Register
-                        </button>
-                    </Link>
-                </section>
+                    : <section className="auth-buttons">
+                        <Link to="/login">
+                            <button className="auth-btn">
+                                Login
+                            </button>
+                        </Link>
+
+                        <Link to="/register">
+                            <button className="auth-btn">
+                                Register
+                            </button>
+                        </Link>
+                    </section>
+                }
             </div>
         </header>
     );
 };
 
-export default Header;
+const mapStateToProps = (state) => ({
+    firebaseUser: state.user.firebaseUser
+});
+
+export default connect(mapStateToProps, null)(Header);
