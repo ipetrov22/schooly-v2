5826fae009cf84c4b0fccf3a75e54c36da0ec675
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-02 23:05:55 +0300

    create PrivateRoute hoc

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import pages from './pages';
+import PrivateRoute from './hoc/PrivateRoute';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { verifyAuth } from './actions/userActions';
@@ -20,7 +21,7 @@ const App = ({ verifyAuth }) => {
               <Route path="/" exact component={pages.Home} />
               <Route path="/login" component={pages.Login} />
               <Route path="/register" component={pages.Register} />
-              <Route path="/question" component={pages.Question} />
+              <PrivateRoute path="/question" component={pages.Question} />
             </Switch>
           </MainLayout>
         </Route>
---
+++ client/src/hoc/PrivateRoute.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ firebaseUser, component: Component, ...rest }) => (
+    <Route {...rest} render={props => (firebaseUser ? <Component {...props} /> :
+        <Redirect to="/" />)} />
+);
+
+const mapStateToProps = (state) => ({
+    firebaseUser: state.user.firebaseUser
+});
+
+export default connect(mapStateToProps, null)(PrivateRoute);
