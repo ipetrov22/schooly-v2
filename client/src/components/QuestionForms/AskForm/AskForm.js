f824382dce0cfed201c765502bf5c04199677c45
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-03 18:23:36 +0300

    add title field

--- client/src/components/QuestionForms/AskForm/AskForm.js
+++ client/src/components/QuestionForms/AskForm/AskForm.js
@@ -6,8 +6,18 @@ const AskForm = () => {
         <div className="question-form-wrapper">
             <form className="form">
                 <article className="icon-container">
-                    <MdSchool size={'35px'} />
+                    <MdSchool size={'45px'} />
                 </article>
+
+                <h2 className="title">Ask a Question</h2>
+
+                <div className="form-control">
+                    <label className="input-label" htmlFor="title">Title</label>
+
+                    <div className="input-field-container">
+                        <input className="input-field" htmlFor="title" name="title" />
+                    </div>
+                </div>
             </form>
         </div>
     );
