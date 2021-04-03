4aadfc274cbc122845fa1d962ba4a7f2e202361a
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-04 00:11:02 +0300

    add grade and description fields in ask form

--- client/src/components/QuestionForms/AskForm/AskForm.js
+++ client/src/components/QuestionForms/AskForm/AskForm.js
@@ -1,5 +1,6 @@
 import { MdSchool } from 'react-icons/md';
 import subjects from '../../../dictionaries/subjects';
+import grades from '../../../dictionaries/grades';
 import './AskForm.scss';
 
 const AskForm = () => {
@@ -18,12 +19,36 @@ const AskForm = () => {
                 </div>
 
                 <div className="form-control">
-                    <select name="subject">
-                        <option value="">Select subject</option>
+                    <select name="subject" className="input-field" placeholder="Subject">
+                        <option className="option-field" defaultValue="">Select a subject</option>
                         {
-                            Object.keys(subjects).map(key => <option value={key}>{subjects[key]}</option>)
+                            Object.keys(subjects)
+                                .map(key => (
+                                    <option key={key} className="option-field" value={key}>{subjects[key]}</option>)
+                                )
                         }
                     </select>
+
+                    <label className="input-label" htmlFor="subject">Subject</label>
+                </div>
+
+                <div className="form-control">
+                    <select name="grade" className="input-field" placeholder="Grade">
+                        <option className="option-field" defaultValue="">Select grade</option>
+                        {
+                            Object.keys(grades)
+                                .map(key => (
+                                    <option key={key} className="option-field" value={key}>{grades[key]}</option>)
+                                )
+                        }
+                    </select>
+
+                    <label className="input-label" htmlFor="grade">Grade</label>
+                </div>
+
+                <div className="form-control">
+                    <textarea className="input-field input-area" htmlFor="description" name="description" placeholder="Description" />
+                    <label className="input-label" htmlFor="description">Description</label>
                 </div>
             </form>
         </div>
