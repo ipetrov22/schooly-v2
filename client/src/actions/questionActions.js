17af2d704c98ba56d52e8af56dac5dc3a670fb02
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-04 23:56:11 +0300

    clean question actions

--- client/src/actions/questionActions.js
+++ client/src/actions/questionActions.js
@@ -1,24 +1,19 @@
 import {
-    CREATE
+    
 } from '../actionTypes/questionTypes';
 
 import {
     createQuestionRequest
 } from '../services/questionService';
 
-export const createSuccess = () => ({
-    type: CREATE
-});
-
-export const createQuestion = (data, idToken) => async (dispatch) => {
+export const createQuestion = (data, idToken) => async () => {
     try {
         const response = await createQuestionRequest(data, idToken);
 
         if (response.error) {
             throw response.error.message;
         }
-        console.log(response);
-        dispatch(createSuccess(response));
+
     } catch (error) {
         throw error;
     }
