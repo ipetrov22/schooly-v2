c23edae55d81ae5aae8c689ea3ed35fefcee4953
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-04 22:22:16 +0300

    server/create Question Model

---
+++ server/src/models/QuestionModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const QuestionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    grade: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: [{
+        type: 'ObjectId',
+        ref: 'User'
+    }],
+    comments: [{
+        type: 'ObjectId',
+        ref: 'Comment'
+    }],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const model = mongoose.model('Question', QuestionSchema);
+module.exports = model;
