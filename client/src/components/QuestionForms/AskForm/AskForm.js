04a763b2272ee2b224d5452de8931486d576fef4
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-04 15:34:32 +0300

    collect ask form data in state

--- client/src/components/QuestionForms/AskForm/AskForm.js
+++ client/src/components/QuestionForms/AskForm/AskForm.js
@@ -1,12 +1,40 @@
 import { MdSchool } from 'react-icons/md';
 import subjects from '../../../dictionaries/subjects';
 import grades from '../../../dictionaries/grades';
+import { useState } from 'react';
 import './AskForm.scss';
 
 const AskForm = () => {
+    const [formData, setFormData] = useState({
+        title: '',
+        subject: '',
+        grade: '',
+        description: ''
+    });
+
+    const onFormChange = (e) => {
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value.trim();
+
+        const newFormData = { ...formData };
+        newFormData[fieldName] = fieldValue;
+        setFormData(newFormData);
+    };
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (formData.title !== ''
+            && formData.subject !== ''
+            && formData.grade !== ''
+            && formData.grade !== '') {
+                console.log(formData)
+        }
+    };
+
     return (
         <div className="question-form-wrapper">
-            <form className="form">
+            <form className="form" onSubmit={onFormSubmit}>
                 <article className="icon-container">
                     <MdSchool size={'45px'} />
                 </article>
@@ -14,12 +42,12 @@ const AskForm = () => {
                 <h2 className="title">Ask a Question</h2>
 
                 <div className="form-control">
-                    <input className="input-field" htmlFor="title" name="title" placeholder="Title" />
+                    <input className="input-field" htmlFor="title" name="title" placeholder="Title" onChange={onFormChange} />
                     <label className="input-label" htmlFor="title">Title</label>
                 </div>
 
                 <div className="form-control">
-                    <select name="subject" className="input-field" placeholder="Subject">
+                    <select name="subject" className="input-field" placeholder="Subject" onChange={onFormChange} >
                         <option className="option-field" defaultValue="">Select a subject</option>
                         {
                             Object.keys(subjects)
@@ -33,7 +61,7 @@ const AskForm = () => {
                 </div>
 
                 <div className="form-control">
-                    <select name="grade" className="input-field" placeholder="Grade">
+                    <select name="grade" className="input-field" placeholder="Grade" onChange={onFormChange} >
                         <option className="option-field" defaultValue="">Select grade</option>
                         {
                             Object.keys(grades)
@@ -47,7 +75,13 @@ const AskForm = () => {
                 </div>
 
                 <div className="form-control">
-                    <textarea className="input-field input-area" htmlFor="description" name="description" placeholder="Description" />
+                    <textarea
+                        className="input-field input-area"
+                        htmlFor="description"
+                        name="description"
+                        placeholder="Description"
+                        onChange={onFormChange}
+                    />
                     <label className="input-label" htmlFor="description">Description</label>
                 </div>
 
