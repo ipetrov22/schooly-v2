23435068fc625b954ee14612edfe75ed97d4069e
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-04 22:34:35 +0300

    create post question controller handler

--- server/src/controllers/questionController.js
+++ server/src/controllers/questionController.js
@@ -1,6 +1,16 @@
 const { Router } = require('express');
 const router = Router();
 
+const verifyIdToken = require('../middlewares/verifyIdToken');
 const { questionService } = require('../services');
+
+router.post('/', verifyIdToken, async (req, res) => {
+    try {
+        const response = await questionService.create();
+        return res.json(response);
+    } catch (error) {
+        return res.status(400).json({ error });
+    }
+});
 
 module.exports = router;
