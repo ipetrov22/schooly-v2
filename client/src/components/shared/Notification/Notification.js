10b1204fdb003e8500f17fe593c929fe997b9480
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-05 23:29:42 +0300

    style success notification

--- client/src/components/shared/Notification/Notification.js
+++ client/src/components/shared/Notification/Notification.js
@@ -1,9 +1,14 @@
+import { IoMdClose } from 'react-icons/io';
 import './Notification.scss';
 
 const Notification = () => {
     return (
-        <div className="notification-container">
-            <p className="message">Some notification message</p>
+        <div className="notification-container success">
+            <p className="message">Some notification message longer tho</p>
+
+            <button className="close-btn">
+                <IoMdClose size="22px" fill="#fff" />
+            </button>
         </div>
     );
 };
