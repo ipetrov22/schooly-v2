e8076a471234b8b84200da5e5785afae4a804129
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-05 17:05:35 +0300

    question card has login button instead of open button for not logged users

--- client/src/components/QuestionList/QuestionCard/QuestionCard.js
+++ client/src/components/QuestionList/QuestionCard/QuestionCard.js
@@ -7,16 +7,24 @@ const QuestionCard = ({ question, isLoggedIn }) => {
         <article className="question-card">
             <section className="date-grade-container">
                 <span className="grade">{question.grade} Grade</span>
-                <span className="date">{new Date(question.date).toString().substring(4,21)}</span>
+                <span className="date">{new Date(question.date).toString().substring(4, 21)}</span>
             </section>
 
             <h2 className="subject">{subjects[question.subject]}</h2>
 
             <p className="title">{question.title}</p>
 
-            <Link to={`/question/${question._id}`}>
-                <button className="open-btn">Open</button>
-            </Link>
+            {
+                isLoggedIn ?
+                    <Link to={`/question/${question._id}`}>
+                        <button className="open-btn">Open</button>
+                    </Link>
+                    :
+                    <Link to={`/login`}>
+                        <button className="open-btn disabled">Login to open</button>
+                    </Link>
+
+            }
         </article>
     );
 };
