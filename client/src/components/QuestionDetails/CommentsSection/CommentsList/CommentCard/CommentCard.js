ed664d37a1706150310a044d268b916d7e8d497d
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-07 13:55:40 +0300

    remove comment edit button

--- client/src/components/QuestionDetails/CommentsSection/CommentsList/CommentCard/CommentCard.js
+++ client/src/components/QuestionDetails/CommentsSection/CommentsList/CommentCard/CommentCard.js
@@ -1,6 +1,5 @@
 import { FaUserCircle } from 'react-icons/fa';
 import transformDate from '../../../../../helpers/transformDate';
-import { AiFillEdit } from 'react-icons/ai';
 import { MdDelete } from 'react-icons/md';
 import './CommentCard.scss';
 
@@ -10,7 +9,6 @@ const CommentCard = ({ comment, firebaseUser, userId }) => {
             {
                 userId === comment.author._id ?
                     <section className="comment-manage-container">
-                        <button className="manage-btn"><AiFillEdit size="25px" fill="#3f51b5" /></button>
                         <button className="manage-btn" >
                             <MdDelete size="25px" fill="#ff0000" />
                         </button>
