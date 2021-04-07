b06f2f04f14938f184a5ac26b417e202dfbca980
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-07 13:34:08 +0300

    add edit and delete buttons for the author of the comment

--- client/src/components/QuestionDetails/CommentsSection/CommentsList/CommentCard/CommentCard.js
+++ client/src/components/QuestionDetails/CommentsSection/CommentsList/CommentCard/CommentCard.js
@@ -1,10 +1,24 @@
 import { FaUserCircle } from 'react-icons/fa';
 import transformDate from '../../../../../helpers/transformDate';
+import { AiFillEdit } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
 import './CommentCard.scss';
 
-const CommentCard = ({ comment }) => {
+const CommentCard = ({ comment, firebaseUser, userId }) => {
     return (
         <article className="comment-card">
+            {
+                userId === comment.author._id ?
+                    <section className="comment-manage-container">
+                        <button className="manage-btn"><AiFillEdit size="25px" fill="#3f51b5" /></button>
+                        <button className="manage-btn" >
+                            <MdDelete size="25px" fill="#ff0000" />
+                        </button>
+                    </section>
+                    :
+                    null
+            }
+
             <section className="icon-container">
                 <FaUserCircle size="25px" />
             </section>
--- client/src/components/QuestionDetails/CommentsSection/CommentsList/CommentsList.js
+++ client/src/components/QuestionDetails/CommentsSection/CommentsList/CommentsList.js
@@ -2,18 +2,27 @@ import { connect } from 'react-redux';
 import CommentCard from './CommentCard';
 import './CommentsList.scss';
 
-const CommentsList = ({ question }) => {
+const CommentsList = ({ question, firebaseUser, userId }) => {
     return (
         <section className="comments-list-container">
             {
-                question.comments.map(x => <CommentCard key={x._id} comment={x} />)
+                question.comments.map(x => (
+                    <CommentCard
+                        key={x._id}
+                        comment={x}
+                        firebaseUser={firebaseUser}
+                        userId={userId}
+                    />
+                ))
             }
         </section>
     );
 };
 
 const mapStateToProps = (state) => ({
-    question: state.question.question
+    question: state.question.question,
+    firebaseUser: state.user.firebaseUser,
+    userId: state.user._id
 });
 
 export default connect(mapStateToProps, null)(CommentsList);
