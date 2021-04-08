b81c149d38ff9cfac08332c8be0f5d13ad8a14d9
Ivaylo Petrov <ivaylo020@gmail.com>
2021-04-08 23:14:30 +0300

    client/add production server address

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 const serverAddress = {
     development: 'http://localhost:3333',
-    production: ''
+    production: 'https://schooly-v2.herokuapp.com'
 };
 
 export const SERVER_ADDRESS = serverAddress[process.env.NODE_ENV];
